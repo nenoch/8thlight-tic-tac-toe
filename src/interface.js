5ad6694ba807ac0bacc5c8f05b29679121d614ea
Irene Canuti <irene.canuti@gmail.com>
2017-04-13 11:24:15 +0100

    User can decide who moves first
    - Implement feature in the interface

--- src/interface.js
+++ src/interface.js
@@ -1,40 +1,50 @@
 $(document).ready(function() {
+  $("#board").hide();
 
-  $("#form").submit(function(event){
+
+  $("#start-form").submit(function(event){
     event.preventDefault();
-    var player1name = $("input#player1-name").val();
-    var player2name = $("input#player2-name").val();
-    var player1symbol = $("input#player1-symbol").val();
-    var player2symbol = $("input#player2-symbol").val();
+    var playerAname = $("input#playerA-name").val();
+    var playerBname = $("input#playerB-name").val();
+    var playerAsymbol = $("input#playerA-symbol").val();
+    var playerBsymbol = $("input#playerB-symbol").val();
 
-    $("#welcome-message").text("It's Tic-Tac-Toe time for " + player1name + " and " + player2name + "!");
-    $("#form").hide();
+    $("#welcome-message").text("It's Tic-Tac-Toe time for " + playerAname + " and " + playerBname + "!");
+    $("#start-form").hide();
 
-    var player1 = new User(player1name, player1symbol);
-    var player2 = new User(player2name, player2symbol);
-
+    var player1 = new User(playerAname, playerAsymbol);
+    var player2 = new User(playerBname, playerBsymbol);
     var game = new Game(player1, player2);
 
-    $(".field").click(function(){
+    $("#first-move-form").html("<label>Would you like to choose who moves first?</label><br /><input type='radio' id='playerA' name='first-mover' value='0'> "+playerAname+"<br /><input type='radio' id='playerB' name='first-mover' value='1'> "+playerBname+"<br /><input type='radio' name='first-mover' value='0'> I don't care<br /><input type='submit' value='Play!'>");
+    $("#first-move-form").submit(function(event){
+      event.preventDefault();
+      var firstPlayerIndex = $("input[name='first-mover']:checked").val();
+      game.firstMover(game.players[firstPlayerIndex])
+      $("#first-move-form").hide();
 
-      var num = Number($(this).attr('id'));
-      var id = $(this).attr('id');
+      $("#board").show();
 
-      game.pickAfield(num);
-      $(this).text(game._currentBoard[num]);
+      $(".field").click(function(){
 
-      $("#end-of-game").text(game.declareWinner());
-      $("#end-of-game").text(game.gameOver());
-      $("#update").text(game._currentPlayer.name + "'s Turn!");
-      var div = document.getElementById("end-of-game")
-      if (div.innerHTML == game.gameOver() || game.declareWinner()) {
-        $("#update").hide();
-        $('.field').unbind('click');
-        var button = '<button type="button" onclick="location.reload()" class="btn btn-primary btn-lg">Start a New Game</button>';
-        $(button).insertAfter('#end-of-game')
-      }
+        var num = Number($(this).attr('id'));
+        var id = $(this).attr('id');
+
+        game.pickAfield(num);
+        $(this).text(game._currentBoard[num]);
+
+        $("#end-of-game").text(game.declareWinner());
+        $("#end-of-game").text(game.gameOver());
+        $("#update").text(game._currentPlayer.name + "'s Turn!");
+        var div = document.getElementById("end-of-game")
+        if (div.innerHTML == game.gameOver() || game.declareWinner()) {
+          $("#update").hide();
+          $('.field').unbind('click');
+          var button = '<button type="button" onclick="location.reload()" class="btn btn-primary btn-lg">Start a New Game</button>';
+          $(button).insertAfter('#end-of-game')
+        }
+      });
     });
-
 
   });
 
